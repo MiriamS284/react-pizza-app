02d87b48d1beff722246dcf833931656a5251316
Miriam Sparbrod <miriam.sparbrod@gmail.com>
2024-03-11 16:51:39 +0100

    React Router Loaders

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,10 +13,12 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    "no-unused-vars": "off",
+    "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
-    "react/prop-types": "off",
   },
 };
