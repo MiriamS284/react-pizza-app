21f88298074d399d3403e25f8573cbc1b4c69dfc
Miriam Sparbrod <miriam.sparbrod@gmail.com>
2024-03-18 12:55:22 +0100

    Adding Redux and React Router - Modeling the Cart state

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,8 +1,10 @@
+import { useSelector } from 'react-redux';
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+  if (!username) return null;
   return (
-    <div className="hidden text-sm font-semibold md:block">
-      Here comes the UserName
-    </div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
